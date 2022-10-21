cbd4e7274b54ad6689e5e7b617831f1b59e9600b
Luca-Braun <l.braun@s-w-e.com>
2022-10-21 11:52:38 +0200

    [TASK] Use shell script instead of inline script

--- .space.kts
+++ .space.kts
@@ -27,33 +27,21 @@ job("Build and run tests") {
         container(displayName = "Test PHP 7.4", image = "php:7.4-cli") {
             shellScript {
                 interpreter = "/bin/bash"
-                content = """
-                    ./install-composer.sh
-                    ./composer.phar update
-                    php ./vendor/bin/phpunit --configuration ./phpunit.xml
-                """
+                location = "./start-test.sh"
             }
         }
 
         container(displayName = "Test PHP 8.0", image = "php:8.0-cli") {
             shellScript {
                 interpreter = "/bin/bash"
-                content = """
-                    ./install-composer.sh
-                    ./composer.phar update
-                    php ./vendor/bin/phpunit --configuration ./phpunit.xml
-                """
+                location = "./start-test.sh"
             }
         }
 
         container(displayName = "Test PHP 8.1", image = "php:8.1-cli") {
             shellScript {
                 interpreter = "/bin/bash"
-                content = """
-                    ./install-composer.sh
-                    ./composer.phar update
-                    php ./vendor/bin/phpunit --configuration ./phpunit.xml
-                """
+                location = "./start-test.sh"
             }
         }
     }
--- start-test.sh
+++ start-test.sh
@@ -2,20 +2,21 @@ echo "-> Download and install composer"
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
-RESULT=0
 
 if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
     >&2 echo '-> ERROR: Invalid installer checksum'
-    RESULT=1
+    exit 1
 else
     php composer-setup.php --quiet
 fi
 
 rm composer-setup.php
 
-#Abort if the last command failed
-if [ ! $RESULT -eq 0 ]; then
-  echo "-> Unable to install composer"
-  exit 1
-fi
+php ./composer.phar update
+
+[ $? -eq 0 ] || exit 1
+
+php ./vendor/bin/phpunit --configuration ./phpunit.xml
+
+exit $?
