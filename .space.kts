bd886dd4617e332d7f278dee563c1af6212246ac
Luca-Braun <l.braun@s-w-e.com>
2022-10-21 11:41:56 +0200

    [TASK] Added environment variables

--- .space.kts
+++ .space.kts
@@ -8,6 +8,21 @@ job("Build and run tests") {
         }
     }
 
+    container(displayName = "Setup environment variables", image = "php:7.4-cli") {
+        env["SPACE_CLIENT_ID"] = Secrets("space_client_id")
+        env["SPACE_CLIENT_SECRET"] = Secrets("space_client_secret")
+        env["SPACE_URL"] = Params("space_url")
+        shellScript {
+            interpreter = "/bin/bash"
+            content = """
+                touch ${$}JB_SPACE_FILE_SHARE_PATH/.env
+                echo "CLIENT_ID=\"${$}SPACE_CLIENT_ID\"" >> ${$}JB_SPACE_FILE_SHARE_PATH/.env
+                echo "CLIENT_SECRET=\"${$}SPACE_CLIENT_SECRET\"" >> ${$}JB_SPACE_FILE_SHARE_PATH/.env
+                echo "URL=\"${$}SPACE_URL\"" >> ${$}JB_SPACE_FILE_SHARE_PATH/.env
+            """
+        }
+    }
+
     parallel {
         container(displayName = "Test PHP 7.4", image = "php:7.4-cli") {
             shellScript {
