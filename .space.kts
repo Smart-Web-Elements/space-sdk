8157c5bd7701f87d8448929855b8b225f9a34c6b
Luca-Braun <l.braun@s-w-e.com>
2022-10-21 12:14:48 +0200

    [TASK] Changed interpreter

--- .space.kts
+++ .space.kts
@@ -13,12 +13,12 @@ job("Build and run tests") {
         nonZeroExitCode { enabled = false }
     }
 
-    container(displayName = "Setup environment variables", image = "mistermarlu/deployment:php-7.4") {
+    container(displayName = "Setup environment variables", image = "jitesoft/phpunit:7.4") {
         env["SPACE_CLIENT_ID"] = Secrets("space_client_id")
         env["SPACE_CLIENT_SECRET"] = Secrets("space_client_secret")
         env["SPACE_URL"] = Params("space_url")
         shellScript {
-            interpreter = "/bin/bash"
+            interpreter = "/bin/sh"
             content = """
                 touch ${'$'}JB_SPACE_FILE_SHARE_PATH/.env
                 echo "CLIENT_ID=\"${'$'}SPACE_CLIENT_ID\"" >> ${'$'}JB_SPACE_FILE_SHARE_PATH/.env
@@ -31,21 +31,21 @@ job("Build and run tests") {
     parallel {
         container(displayName = "Test PHP 7.4", image = "jitesoft/phpunit:7.4") {
             shellScript {
-                interpreter = "/bin/bash"
+                interpreter = "/bin/sh"
                 location = "./start-test.sh"
             }
         }
 
         container(displayName = "Test PHP 8.0", image = "jitesoft/phpunit:8.0") {
             shellScript {
-                interpreter = "/bin/bash"
+                interpreter = "/bin/sh"
                 location = "./start-test.sh"
             }
         }
 
         container(displayName = "Test PHP 8.1", image = "jitesoft/phpunit:8.1") {
             shellScript {
-                interpreter = "/bin/bash"
+                interpreter = "/bin/sh"
                 location = "./start-test.sh"
             }
         }
