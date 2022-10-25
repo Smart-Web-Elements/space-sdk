0fcb7ea0d3b731765a9f4a3c8a8670849a599742
Luca-Braun <l.braun@s-w-e.com>
2022-10-25 13:19:16 +0200

    [TASK] Fixed composer installation

--- start-test.sh
+++ start-test.sh
@@ -1,4 +1,9 @@
-php composer update
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+php ./composer.phar update
 
 [ $? -eq 0 ] || exit 1
 
@@ -8,7 +13,7 @@ php ./vendor/bin/phpunit --configuration ./phpunit.xml
 
 cd ../space-sdk-builder
 
-php composer update
+php ../space-sdk/composer.phar update
 
 [ $? -eq 0 ] || exit 1
 
@@ -20,7 +25,7 @@ mv ../space-sdk/src ../space-sdk/src-old
 cp -r ./build/src ../space-sdk/src
 
 cd ../space-sdk
-php composer dump-autoload
+php ./composer.phar dump-autoload
 php ./vendor/bin/phpunit --configuration ./phpunit.xml
 
 [ $? -eq 0 ] || exit 1
