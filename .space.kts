4abdb7148386e339d88bd1240919159d013b067e
Luca-Braun <l.braun@s-w-e.com>
2022-10-31 13:17:03 +0100

    [TASK] Added rebuild forcing

--- .space.kts
+++ .space.kts
@@ -27,3 +27,28 @@ job("Build and run generated tests") {
         }
     }
 }
+
+job("Force rebuild and run generated tests") {
+    startOn {
+        // "startOn" ist leer, damit dieser Job nur manuell ausgeführt werden kann.
+    }
+
+    git {
+        refSpec {
+            +"refs/heads/v2"
+        }
+    }
+
+    git("space-sdk-builder")
+
+    container(displayName = "Test PHP 8.1", image = "mistermarlu/testing:8.1") {
+        env["CLIENT_ID"] = Secrets("space_client_id")
+        env["CLIENT_SECRET"] = Secrets("space_client_secret")
+        env["URL"] = Params("space_url")
+        shellScript {
+            interpreter = "/bin/sh"
+            location = "./start-test.sh"
+            args("--force")
+        }
+    }
+}
--- start-test.sh
+++ start-test.sh
@@ -1,3 +1,13 @@
+while test "$#" -gt 0; do
+  case "$1" in
+  --force)
+    export FORCE=true
+    ;;
+  esac
+
+  shift
+done
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
@@ -7,9 +17,11 @@ php ./composer.phar update
 
 [ $? -eq 0 ] || exit 1
 
-php ./vendor/bin/phpunit --configuration ./phpunit.xml
+if [ -z ${FORCE+x} ]; then
+    php ./vendor/bin/phpunit --configuration ./phpunit.xml
 
-[ $? -ne 0 ] || exit 0
+    [ $? -ne 0 ] || exit 0
+fi
 
 chmod +x ../space-sdk-builder/upgrade.sh
 ../space-sdk-builder/upgrade.sh
